808911b102482327b68373b819dad8deaebb1eb4
Dante <github.6p8ng@kironmail.com>
2026-03-18 22:30:54 -0600

    fix: / at wrong spot, oops

--- backend/src/routes/api/mod.rs
+++ backend/src/routes/api/mod.rs
@@ -5,5 +5,5 @@ use crate::config::state::AppState;
 pub mod v1;
 
 pub fn mount() -> Router<AppState> {
-    Router::new().nest("/api", Router::new().nest("v1/", v1::mount()))
+    Router::new().nest("/api", Router::new().nest("/v1", v1::mount()))
 }
