0480e22f5e7c11e2689b8dbaa28e837cda604dff
Dante <github.6p8ng@kironmail.com>
2026-03-30 10:21:27 -0600

    refactor: make a more minimal book card

--- web/src/routes/libraries/[library_id]/media/+page.svelte
+++ web/src/routes/libraries/[library_id]/media/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
-	import { get_cover_url } from '@shiori/api-client';
 	import * as Empty from '$lib/components/ui/empty';
+	import MediaCard from '$lib/components/media-card.svelte';
 
 	import BookText from '@lucide/svelte/icons/book-text';
 
@@ -14,24 +13,7 @@
 			class="grid w-full grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
 		>
 			{#each data.media as media (media.id)}
-				<a
-					href={resolve('/media/[media_id]', { media_id: media.id.toString() })}
-					class="group overflow-hidden rounded-2xl bg-secondary p-2"
-				>
-					<div class="aspect-2/3 w-full overflow-hidden">
-						<img
-							class="h-full w-full rounded-xl object-cover"
-							src={get_cover_url(media.cover_path) ?? ''}
-							alt={`${media.name} cover image`}
-						/>
-					</div>
-
-					<div class="p-2">
-						<h3 class="text-lg font-medium">
-							{media.name}
-						</h3>
-					</div>
-				</a>
+				<MediaCard {media} />
 			{/each}
 		</div>
 	{:else}
