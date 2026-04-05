74d7bdb8c50815160d7fbcb647493c7bff616954
Dante <85536418+daannte@users.noreply.github.com>
2026-04-05 14:48:56 -0600

    feat(svelte): upload books (#47)
    * chore: clippy
    
    * feat: working dropzone
    
    * fix: dont allow files with same name
    
    * feat: i like this dialog
    
    * feat: upload files working

---
+++ web/src/lib/components/upload/dropzone.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import FilePlusCorner from '@lucide/svelte/icons/file-plus-corner';
+	import { Button } from '../ui/button';
+
+	interface Props {
+		files: File[];
+	}
+
+	let { files = $bindable() }: Props = $props();
+	let inputEl: HTMLInputElement;
+	let isDragActive = $state(false);
+
+	function addFiles(newFiles: FileList) {
+		const fileArray = Array.from(newFiles);
+		const existing = new Set(files.map((f) => f.name));
+		const filtered = fileArray.filter((f) => !existing.has(f.name));
+		files = [...files, ...filtered];
+	}
+
+	function handleDrop(e: DragEvent) {
+		e.preventDefault();
+		isDragActive = false;
+
+		let newFiles = e.dataTransfer?.files;
+		if (newFiles) addFiles(newFiles);
+	}
+</script>
+
+<div
+	class={`flex items-center justify-center rounded-lg border-2 border-dotted border-border p-4 ${isDragActive ? 'border-primary bg-muted' : ''}`}
+	role="button"
+	tabindex="0"
+	ondrop={handleDrop}
+	ondragleave={() => (isDragActive = false)}
+	ondragover={(e) => {
+		e.preventDefault();
+		isDragActive = true;
+	}}
+>
+	<div class="flex flex-col items-center text-sm">
+		<FilePlusCorner class="text-muted-foreground" size={48} />
+
+		<div class="mt-2 text-center">
+			<p>Drag & drop files here or</p>
+
+			<p class="-mt-2">
+				<Button variant="link" class="p-0 text-chart-1" onclick={() => inputEl.click()}
+					>choose files</Button
+				>{' '}
+				to upload
+			</p>
+		</div>
+	</div>
+</div>
+
+<input
+	id="fileUpload"
+	type="file"
+	bind:this={inputEl}
+	multiple
+	hidden
+	accept="application/epub+zip"
+	onchange={(e: Event) => {
+		const target = e.target as HTMLInputElement;
+		if (target.files) addFiles(target.files);
+	}}
+/>
--- web/src/routes/libraries/[library_id]/media/+page.svelte
+++ web/src/routes/libraries/[library_id]/media/+page.svelte
@@ -1,30 +1,34 @@
 <script lang="ts">
 	import MediaCard from '$lib/components/media-card.svelte';
 	import EmptyView from '$lib/components/empty-view.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	import BookText from '@lucide/svelte/icons/book-text';
+	import UploadDialog from '$lib/components/upload/upload-dialog.svelte';
 
 	let { data } = $props();
+
+	let isUploadOpen = $state(false);
 </script>
 
-<div class="flex p-6">
-	{#if data.media.length > 0}
-		<div
-			class="grid w-full grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
-		>
-			{#each data.media as media (media.id)}
-				<MediaCard {media} />
-			{/each}
-		</div>
-	{:else}
-		<EmptyView
-			title="No media found"
-			description="Upload your favorite media to get started"
-			icon={BookText}
-		>
-			{#snippet content()}
-				<p>TODO add button to upload media</p>
-			{/snippet}
-		</EmptyView>
-	{/if}
-</div>
+{#if data.media.length > 0}
+	<div
+		class="grid w-full grid-cols-2 gap-6 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
+	>
+		{#each data.media as media (media.id)}
+			<MediaCard {media} />
+		{/each}
+	</div>
+{:else}
+	<EmptyView
+		title="No media found"
+		description="Upload your favorite media to get started"
+		icon={BookText}
+	>
+		{#snippet content()}
+			<Button size="lg" onclick={() => (isUploadOpen = true)}>Upload Files</Button>
+		{/snippet}
+	</EmptyView>
+{/if}
+
+<UploadDialog id={data.libraryId} bind:isOpen={isUploadOpen} />
