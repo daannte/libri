ad5dcb78b755d384362debda90e2b46ac4125545
Dante <github.6p8ng@kironmail.com>
2026-04-08 18:36:00 -0600

    chore: prettier and eslint

--- web/src/lib/components/library-header.svelte
+++ web/src/lib/components/library-header.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { page } from '$app/state';
-
 	import { Button } from './ui/button';
 
 	import Upload from '@lucide/svelte/icons/upload';
@@ -10,9 +8,6 @@
 	}
 
 	let { isOpen = $bindable() }: Props = $props();
-
-	let id = $derived(page.params.library_id);
-	$inspect(id);
 </script>
 
 <header class="sticky top-0 flex w-full border-b border-border p-2">
