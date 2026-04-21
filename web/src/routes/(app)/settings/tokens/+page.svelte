61db4dda1defa3256a6e966b2dc653c466ce27b3
Dante <85536418+daannte@users.noreply.github.com>
2026-04-21 12:12:11 -0600

    feat(web): settings page (#81)

--- web/src/routes/(app)/settings/tokens/+page.svelte
+++ web/src/routes/(app)/settings/tokens/+page.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
+	import { Label } from '@shiori/components';
+
 	import CreateDialog from '$lib/components/tokens/create-dialog.svelte';
 	import DataTable from '$lib/components/tokens/data-table.svelte';
 
 	let { data } = $props();
 </script>
 
-<div class="p-4">
-	<div class="flex justify-between">
-		<div class="">
-			<h1 class="text-lg font-medium md:text-xl">Tokens List</h1>
-			<p class="text-sm">Manage your API tokens</p>
-		</div>
-		<CreateDialog />
-	</div>
-	<DataTable data={data.tokens} />
+<div class="flex items-center justify-between">
+	<Label class="text-base md:text-lg">Tokens List</Label>
+	<CreateDialog />
 </div>
+<DataTable data={data.tokens} />
