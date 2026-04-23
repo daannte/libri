8db888e89129b65a5766b79fd3ecfe9f1de2f56a
Dante <github.6p8ng@kironmail.com>
2026-04-23 10:34:36 -0600

    refactor: change route setup a little

--- tests/openapi.rs
+++ tests/openapi.rs
@@ -2,6 +2,8 @@ use shiori::BaseOpenApi;
 
 #[test]
 fn test_open_api_snapshot() {
-    let (_, newest) = BaseOpenApi::build();
+    let core = ShioriCore::new();
+    let app = Arc::new(core.get_app());
+    let (_, newest) = BaseOpenApi::build(app);
     insta::assert_snapshot!(newest.to_pretty_json().unwrap())
 }
